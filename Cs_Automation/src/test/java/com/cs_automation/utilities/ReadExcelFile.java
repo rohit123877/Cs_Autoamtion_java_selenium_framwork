941d6160785bdb704c8acb222cebdf09915b76fd
rohit123877 <rohitkoshal44@gmail.com>
2026-02-28 21:49:27 +0530

    Add new Read excell file class for data driven testing

--- Cs_Automation/src/test/java/com/cs_automation/utilities/ReadExcelFile.java
+++ Cs_Automation/src/test/java/com/cs_automation/utilities/ReadExcelFile.java
@@ -1,6 +1,7 @@
 package com.cs_automation.utilities;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -8,103 +9,27 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcelFile {
+	
 
-
-		public static FileInputStream inputStream;
-		public static XSSFWorkbook workBook;
-		public static XSSFSheet excelSheet;
-		public static XSSFRow row;
-		public static XSSFCell cell;
-
-		public static String getCellValue(String fileName, String sheetName, int rowNo, int cellNo/*column no.*/)
-		{
-
-			try
-			{
-
-				inputStream = new FileInputStream(fileName);
-				workBook = new XSSFWorkbook(inputStream);
-				excelSheet =  workBook.getSheet(sheetName);
-				cell = workBook.getSheet(sheetName).getRow(rowNo).getCell(cellNo);
-
-				workBook.close();
-
-				return cell.getStringCellValue();
-
-			}
-
-			catch (Exception e)
-
-			{
-				return "";
-
-			}
+	//Classes use for red the excel file 
+	public static FileInputStream inputStream;
+	public static XSSFWorkbook workBook;
+	public static XSSFSheet excelSheet;
+	public static XSSFRow row;
+	public static XSSFCell cell;
+	public static String getCellValue(String fileName , String sheetName, int rowNO, int CellNo)
+	{
+		try {
+			inputStream = new FileInputStream(fileName);
+			workBook = new XSSFWorkbook(inputStream);
+			excelSheet = workBook.getSheet(sheetName);
+			cell = workBook.getSheet(sheetName).getRow(rowNO).getCell(CellNo);
+			workBook.close();
+			return cell.getStringCellValue();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return "";
 		}
-
-		public static int getRowCount (String fileName, String sheetName)
-		{
-
-			try
-
-			{
-
-				inputStream = new FileInputStream(fileName);
-				//create XSSFWorkBook Class object for excel file manipulation
-
-				workBook = new XSSFWorkbook(inputStream);
-				excelSheet =  workBook.getSheet(sheetName);
-
-				//get total no. of rows
-				int ttlRows = excelSheet.getLastRowNum() + 1;
-
-				workBook.close();
-
-				return ttlRows;
-
-			}
-
-			catch (Exception e)
-
-			{
-
-				return 0;
-
-			}
-
-		}
-
-		public static int getColCount (String fileName, String sheetName)
-		{
-
-			try
-
-			{
-
-				FileInputStream inputStream = new FileInputStream(fileName);
-				//create XSSFWorkBook Class object for excel file manipulation
-				XSSFWorkbook workBook = new XSSFWorkbook(inputStream);
-				XSSFSheet excelSheet =  workBook.getSheet(sheetName);
-
-				//get total no. of columns
-				int ttlCells = excelSheet.getRow(0).getLastCellNum();
-
-				workBook.close();
-				return ttlCells;
-
-			}
-
-			catch (Exception e)
-
-			{
-
-				return 0;
-
-			}
-
-		}
-
 	}
-
-
-
-
+}
